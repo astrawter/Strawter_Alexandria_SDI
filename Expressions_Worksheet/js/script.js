8e66e49315d9cc4ecb5c6d48b6a54363e3b980f7
astrawter <afstrawter@gmail.com>
2015-08-31 17:11:39 -0500

    Dog Years
    Completed the first problem regarding Sparky’s age in dog years for the
    expression worksheet assignment

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,15 @@
+/*
+Alexandria Strawter
+08/31/15
+Expressions Worksheet
+ */
+
+
+//Dog Years : Calculate Sparky’s age in dog years
+var humanYears = 1;
+var dogYears = 7;
+//Multiply his 1 human year by 7 dog years
+var age = humanYears * dogYears;
+//print out Sparky’s age
+console.log("The actual age of Sparky is " + age + " in dog years");
+
