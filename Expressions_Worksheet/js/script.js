8734394e4e3f7422c29df67adfc144b0fc6c3c58
astrawter <afstrawter@gmail.com>
2015-08-31 17:30:13 -0500

    Slice of Pie part 1
    Completed the expression for the slice of pie part 1 problem.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -11,5 +11,28 @@ var dogYears = 7;
 //Multiply his 1 human year by 7 dog years
 var age = humanYears * dogYears;
 //print out Sparky’s age
-console.log("The actual age of Sparky is " + age + " in dog years");
+console.log("Sparky is " + humanYears + " human year old which is "+ age + " in dog years.");
 
+
+//Slice of Pie part 1: Determine the number of slices per person if the following is true:
+//Number of slices per pizza: 12
+var slicesPizza = 12;
+//Number of people at the party: 15
+var people = 15;
+//Number of pizzas ordered: 5
+var wholePizza = 5;
+//Multiply the slices per pizza by the amount of pizza ordered
+var whole =slicesPizza * wholePizza;
+// Divide the total by number of people at party to get number of slices per person
+var slices = whole / people;
+//Print out how many slices each person will receive //“Each person ate X slices of pizza at the party.”
+console.log("Each person ate " + slices + " slices of pizza at the party.");
+
+
+//Slice of Pie part 2: Determine the number of leftover slices Sparky will have
+//Number of slices per pizza: 12
+//Number of people at the party: 15
+//Number of pizzas ordered: 5
+
+//Number of slices Sparky gets to eat.
+//Sparky got X slices of pizza.”
