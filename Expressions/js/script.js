ab7e9933f75c2950d7743826e5feac481913c586
astrawter <afstrawter@gmail.com>
2015-08-31 12:58:57 -0500

    Created an Arithmetic Operator
    Created the equation for the area of a triangle and tested different
    values

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -5,8 +5,16 @@ var a =2; //defines variable
 var b;
 b= a + 3; //adds 3 to a
 console.log(b);
-*/
+
 
 var yearBorn = 1993;
 var age = 2015 - yearBorn;
 console.log(age);
+ */
+
+//area id triangle half the width x the height
+var width = 23;
+var height = 17;
+
+var area =width * height/2;
+console.log(area);
