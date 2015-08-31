ec08ceb7c81ce650a0742537077b9bd3a080b5f7
astrawter <afstrawter@gmail.com>
2015-08-31 11:38:55 -0500

    Added an Array
    Added an Array and learned how they work and useful tricks to use for
    my next assignments!

--- Values_Variables/js/script.js
+++ Values_Variables/js/script.js
@@ -2,13 +2,19 @@
 /*
 var yearBorn = 1993; //Declare and define on one line
 var age = 21;
-*/
 var isStudent = false; //Boolean
 var firstName ="Alexandria";
 var phrase ="I don't know ask the professor";
-/*
 cconsole.log("Hello World!")
 console.log(yearBorn);
  */
 
 //alert("Hello World!");
+
+//Arrays
+var num = 2;
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0, 1 , 2, 3
+muppetNames[4] = "Fozzy";
+muppetNames[1] = "Rizzo";
+
+console.log(muppetNames[num]);
