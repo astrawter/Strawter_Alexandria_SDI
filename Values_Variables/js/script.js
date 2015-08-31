ccac228acf150f163a6d21be2b9a4eed00d74fe3
astrawter <afstrawter@gmail.com>
2015-08-31 11:18:43 -0500

    Setup HTML and JS files
    Setup HTML and JS files also played with Values and Variables

---
+++ Values_Variables/js/script.js
@@ -0,0 +1,11 @@
+
+var yearBorn = 1993; //Declare and define on one line
+var age; //declare
+age = 21; //define
+/*
+
+cconsole.log("Hello World!")
+console.log(yearBorn);
+ */
+
+//alert("Hello World!");
