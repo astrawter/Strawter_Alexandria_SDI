4fca519090b6d2250e504fcb8246799369028e0c
astrawter <afstrawter@gmail.com>
2015-08-31 11:30:45 -0500

    Added Boolean and Strings
    Added Boolean and Strings testing out other js skills

--- Values_Variables/js/script.js
+++ Values_Variables/js/script.js
@@ -1,9 +1,12 @@
 
-var yearBorn = 1993; //Declare and define on one line
-var age; //declare
-age = 21; //define
 /*
-
+var yearBorn = 1993; //Declare and define on one line
+var age = 21;
+*/
+var isStudent = false; //Boolean
+var firstName ="Alexandria";
+var phrase ="I don't know ask the professor";
+/*
 cconsole.log("Hello World!")
 console.log(yearBorn);
  */
