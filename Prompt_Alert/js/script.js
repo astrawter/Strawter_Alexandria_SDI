ef68a037067c74969b84aabec7a2a83a4f37876e
astrawter <afstrawter@gmail.com>
2015-08-31 16:30:01 -0500

    Prompts and Alerts
    Discovering how to use Prompts and Alerts together and to store input

---
+++ Prompt_Alert/js/script.js
@@ -0,0 +1,11 @@
+/*var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+*/
+var width = prompt("What is the width of the rectangle?"); //asks for width
+var height = prompt("Enter the height");//asks for heightt
+
+var area = width * height; //calculates
+console.log(area); //prints it out
+
+var result = "the area of your rectangle is" + " " +area + " sq ft.";
+alert(result);
