a346daab1758757783eb1de65d61db4f33c286cb
astrawter <afstrawter@gmail.com>
2015-09-02 07:33:49 -0500

    Made my first conditional example
    beginning the code for a conditional to determine if they are old
    enough to ride the coaster.

---
+++ Conditonals_Example/js/script.js
@@ -0,0 +1,9 @@
+//Conditonal Logic
+
+var oldEnough = true;
+//if the child is old enough print to console "You can ride!"
+if(oldEnough)
+//Code performed if the condition is true
+    {
+    console.log("You can ride the coaster!")
+    }
