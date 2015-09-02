e5fafd8e1a6850c8be67b648fd8ad795ff3605a1
astrawter <afstrawter@gmail.com>
2015-09-02 07:46:44 -0500

    else if
    Worked with else if statement to determine how to choose between three
    options in a conditional!

--- Conditonals_Example/js/script.js
+++ Conditonals_Example/js/script.js
@@ -1,14 +1,19 @@
 //Conditonal Logic
 
-var kidHeight = 30;
+var kidHeight = 47;
 var minHeight = 48;
+var parentHeight = 45
+
 //if the child is old enough print to console "You can ride!"
 if(kidHeight > minHeight)
 //Code performed if the condition is true
     {
     console.log("You can ride the coaster!")
     }
-else(kidHeight < minHeight)
-{
+else if(parentHeight)
+    {
+    console.log("You can ride with a parent.")
+    }
+else{
     console.log("Sorry you cannot ride. :/")
-}
+    }
