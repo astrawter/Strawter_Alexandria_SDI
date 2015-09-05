314e09e9294b46145fb376ce0068e65f187ec74b
astrawter <afstrawter@gmail.com>
2015-09-05 10:18:08 -0500

    Added Commentary
    Added commentary as to what is being done within the script.

--- Strawter_Alexandria_Expressions_Assignment/js/script.js
+++ Strawter_Alexandria_Expressions_Assignment/js/script.js
@@ -15,19 +15,19 @@ Expressions Assignment
 //test values in the comments
 
 
-
+//Alert user of the calculations we will be doing
 alert("We are going to figure out the total price of the last 4 items you recently purchased. \nWe will then calculate the average price of these items.");
-//The user is prompted for at least 3 values, and numbers are correctly parsed or casted.
+//The user is prompted to enter 4 price values
 var priceOne = prompt("Enter the price of your first item.");
 var priceTwo = prompt("Enter the price of your second item.");
 var priceThree = prompt("Enter the price of your third item.");
 var priceFour = prompt("Enter the price of your fourth item.");
-
+//place the items into an array to store the values
 var items = [priceOne, priceTwo, priceThree, priceFour];
-console.log("Your items are priced at " + items + " each.");
-
+console.log("Your items are priced at " + items + " individually.");
+//calculate the total price of the 4 items entered
 var total= Number(items[0]) + Number(items [1]) + Number(items [2]) + Number(items [3]);
 console.log(total);
-
+//calculate the average of the items as well
 var Average = total /=4;
 console.log(Average);
