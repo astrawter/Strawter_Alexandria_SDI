467095573b4c1005fc8ef1a02d385ea1856522a8
astrawter <afstrawter@gmail.com>
2015-09-05 10:31:42 -0500

    Added Test Values and Catenation
    Added Catenation to make the alerts more descriptive and also entered
    my test values to make sure the calculator worked correctly.

--- Strawter_Alexandria_Expressions_Assignment/js/script.js
+++ Strawter_Alexandria_Expressions_Assignment/js/script.js
@@ -6,28 +6,31 @@ Expressions Assignment
  */
 
 
-
-//least 2 factors as variables add total and multiply, not hard-coded values, and at least 2 arithmetic operators (total, not in one place).
-
-//at least one array, string, number
-//Console.log & alerts that include calculate values with descriptive text and catenation
-// one of the following (+=, -=,*=,/=, ++, --)
-//test values in the comments
-
-
 //Alert user of the calculations we will be doing
+var end ="Thank you for testing my calculator!"
 alert("We are going to figure out the total price of the last 4 items you recently purchased. \nWe will then calculate the average price of these items.");
 //The user is prompted to enter 4 price values
 var priceOne = prompt("Enter the price of your first item.");
 var priceTwo = prompt("Enter the price of your second item.");
 var priceThree = prompt("Enter the price of your third item.");
 var priceFour = prompt("Enter the price of your fourth item.");
+/* For test values I entered 10,10,10,& 10*/
 //place the items into an array to store the values
 var items = [priceOne, priceTwo, priceThree, priceFour];
+//print to console the prices entereed
 console.log("Your items are priced at " + items + " individually.");
 //calculate the total price of the 4 items entered
 var total= Number(items[0]) + Number(items [1]) + Number(items [2]) + Number(items [3]);
-console.log(total);
-//calculate the average of the items as well
-var Average = total /=4;
-console.log(Average);
+/* For the test values I entered I was given a total of 40 which is correct*/
+//print to console and alert the user of the total
+console.log("The total of the four items combined is $" + total);
+alert("The total of the four items combined is $" + total);
+//create a variable for the number of items to divide by
+var divide= 4;
+//calculate the average of the items
+/* For the test values I entered I was given an average of 10 which is also correct*/
+var Average = total /=divide;
+//print to console and alert the user of the average
+console.log("The average of the items is $" + Average);
+alert("The average of the items is $" + Average);
+alert(end);
