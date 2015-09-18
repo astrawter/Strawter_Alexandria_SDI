372c16247b0c3620d094b2be76e2103fd4607100
astrawter <afstrawter@gmail.com>
2015-09-17 20:56:12 -0500

    Values and validation
    Prompted for the values to be used in the calculations and also
    validated that an entry was made.

--- Strawter_Alexandria_Conditionals_Assignment/js/script.js
+++ Strawter_Alexandria_Conditionals_Assignment/js/script.js
@@ -5,11 +5,24 @@
  9-16-2015
  */
 
-//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values.
+//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values. Prompted user for values also checked to make sure a value was entered.
 var num1 = prompt("We are going to calculate if the total of 3 random numbers is even or odd after being multiplied by 3. \nEnter the first number");
+if(num1 === ""){
+    num1 = prompt("Please enter a value")
+};
 var num2 = prompt("Now for the second number.");
+if(num2 === ""){
+    num2 = prompt("Please enter a value")
+};
 var num3 = prompt("Finally your last number.");
+if(num3 === ""){
+    num3 = prompt("Please enter a value")
+};
+var add = [Number(num1) + Number(num2)+ Number(num3)];//added the totals of the entered values
+alert("Your three numbers equal " + add);//alerted the user of the total of the entered values
 
+var total = add *3;//multiplied the total by 3
+alert("After being multiplied by three the total is " + total);//alerted user of the total
 
 
 
