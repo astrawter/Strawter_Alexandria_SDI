c72f35c1e0bc052c6bc0fcf13c9a4937ea722766
astrawter <afstrawter@gmail.com>
2015-09-17 20:23:03 -0500

    Prompted the user
    Prompted the user to enter in 3 numbers for the calculations

--- Strawter_Alexandria_Conditionals_Assignment/js/script.js
+++ Strawter_Alexandria_Conditionals_Assignment/js/script.js
@@ -5,3 +5,28 @@
  9-16-2015
  */
 
+//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values.
+var num1 = prompt("We are going to calculate if the total of 3 random numbers is even or odd after being multiplied by 3. \nEnter the first number");
+var num2 = prompt("Now for the second number.");
+var num3 = prompt("Finally your last number.");
+
+
+
+
+
+//2 Conditional Statements that are NOT part of prompt validation are present in code and work correctly.
+
+//At least 1 Else If Statement is present in code and works correctly.
+
+//At least 1 Ternary Statement is present in code and works correctly.
+
+//At least 1 Logical Operator is present in code and works correctly.
+
+//3 user prompts are correctly used in the code.
+
+//All prompts are validated and checked to verify values using conditionals
+
+//Each line of code has a meaningful comment.
+
+//Test values are present in a comment at the end of code and are correct.
+
